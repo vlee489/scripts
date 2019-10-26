1f289835fbe61ac775c83bc9ef2ce1a20a587ee6
Vincent Lee <vlee@vlee.me.uk>
2019-10-26 07:47:24 +0100

    Create MozfestDeploy.sh

---
+++ MozfestDeploy.sh
@@ -0,0 +1,3 @@
+apt-get install python-numpy python-wxgtk3.0 python-pyparsing python-cairo libhidapi-libusb0 -y
+wget https://redfernelectronics.co.uk/?ddownload=8210
+dpkg -i crumble_1.0.13_all.deb
